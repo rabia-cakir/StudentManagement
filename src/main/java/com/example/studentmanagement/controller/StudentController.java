ce259b3a61fe97a62e64caa575cfe512ac697744
rabia-cakir <rabiacakir0681@gmail.com>
2022-02-18 22:00:18 +0300

    contoller is created

---
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -0,0 +1,8 @@
+package com.example.studentmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+
+//the controller annotation indicates that this particular class serves the role of a controller
+@Controller
+public class StudentController {
+}
