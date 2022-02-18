da90570f22505e3778f473c1dcd42d979720c3a7
rabia-cakir <rabiacakir0681@gmail.com>
2022-02-18 21:28:44 +0300

    project structure

---
+++ src/main/java/com/example/studentmanagement/StudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementApplication.class, args);
+    }
+
+}
