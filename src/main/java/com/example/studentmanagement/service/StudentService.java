e5a0ff59497abe3433b2199467dc99ceb3696ee5
rabia-cakir <rabiacakir0681@gmail.com>
2022-02-18 21:58:04 +0300

    service layer is created

---
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -0,0 +1,4 @@
+package com.example.studentmanagement.service;
+
+public interface StudentService {
+}
---
+++ src/main/java/com/example/studentmanagement/service/impl/StudentServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.studentmanagement.service.impl;
+
+import com.example.studentmanagement.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+}
