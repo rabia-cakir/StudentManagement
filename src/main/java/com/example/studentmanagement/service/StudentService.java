c350eb5125714f24cf4cf545a4c34d7472067565
rabia-cakir <rabiacakir0681@gmail.com>
2022-02-19 12:26:06 +0300

    adding service for delete

--- src/main/java/com/example/studentmanagement/service/StudentService.java
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -11,4 +11,6 @@ public interface StudentService {
 
     Student getStudentById(Long id);
     Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
 }
--- src/main/java/com/example/studentmanagement/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/studentmanagement/service/impl/StudentServiceImpl.java
@@ -38,4 +38,9 @@ public class StudentServiceImpl implements StudentService {
     public Student updateStudent(Student student) {
         return studentRepository.save(student);
     }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
 }
